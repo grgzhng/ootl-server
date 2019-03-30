8f46d4b4b84d0ee828e6fa3d27178eec8ab9693f
George Zhang <georgezhang@ucla.edu>
2019-03-30 10:31:30 -0700

    boilerplate parser

---
+++ parse_news.js
@@ -0,0 +1,7 @@
+function parse_news(res) {
+  console.log(res);
+}
+
+module.exports = {
+  parse_news: parse_news
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
+const parser = require("./parse_news");
 
 const TABOOLA_API =
   "https://us-central1-vision-migration.cloudfunctions.net/la_hacks_2019?market_code=0";
@@ -9,20 +10,25 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.post("/lahacks", (req, res) => {
-  var inbMsg = req.body.Body.toLowerCase().trim();
+  let inbMsg = req.body.Body.toLowerCase().trim();
   if (inbMsg == "ootl") {
     axios
       .get(TABOOLA_API)
       .then(response => {
-        console.log(response);
-        // res.send(response.data.status);
+        // console.log(response.data.buckets[0].report.rollups[0]);
+        parser.parse_news(response.data.buckets[0].report.rollups[0]);
+        res.send(
+          `<Response><Message>${
+            response.data.buckets[0].report.rollups[0].name
+          }</Message></Response>`
+        );
       })
       .catch(error => {
         console.log(error);
       });
   } else {
     res.send(
-      "<Response><Message>invalid response: text \"ootl\" to recieve today's news!</Message></Response>"
+      '<Response><Message>invalid response: text "ootl" to recieve today\'s news!</Message></Response>'
     );
   }
 });
