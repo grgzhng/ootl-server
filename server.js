6c9272e6ae225533c6a3f659bdbbb462f495f0e1
Justin Jeon <justinjeon151@gmail.com>
2019-03-29 23:46:47 -0700

    example twilio

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
-const http = require("http");
-const express = require("express");
-const MessagingResponse = require("twilio").twiml.MessagingResponse;
+var express = require("express");
+var bodyParser = require("body-parser");
+var dotenv = require("dotenv");
+dotenv.load();
+var fromNum = "+16502156223";
 
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.post("/lahacks", (req, res) => {
+  var inbMsg = req.body.Body.toLowerCase().trim();
+  if (inbMsg == "taro") {
+    res.send(
+      "<Response><Message>you right. promo: LAHACKS19, @lizziepika, lsiegle@twilio.com</Message></Response>"
+    );
+  }
+  res.send(
+    "<Response><Message>meh. promo: LAHACKS19, @lizziepika, lsiegle@twilio.com</Message></Response>"
+  );
 });
+app.listen(8080, () => console.log("listening"));
 
-app.listen(5000, () => {
-  console.log("Example app listening on port 8000!");
-});
+const client = require("twilio")(YOUR - ACCOUNT - SID, YOUR - AUTH - TOKEN);
+var filter = {
+  to: fromNum
+};
+client.messages.each(filter, msg =>
+  client.calls
+    .create({
+      url: "http://demo.twilio.com/docs/classic.mp3",
+      to: msg.from,
+      from: fromNum
+    })
+    .then(call => console.log(call.sid))
+);
